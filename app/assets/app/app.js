6386cdbc8f0a04ae4db0bac3916b6b2940f4ab7c
Amir Savand <amir@savandbros.com>
2018-08-21 04:45:24 +0400

    Add arrow and update skills

--- app/assets/app/app.js
+++ app/assets/app/app.js
@@ -1,14 +1,14 @@
 /**
  * Toggle for editor
  */
-setInterval(function() {
+setInterval(function () {
   $("#editor b").toggle();
 }, 500);
 
 /**
  * Resize the editor to full screen or minimal based on scroll
  */
-$(document).on("ready scroll", function() {
+$(document).on("ready scroll", function () {
   if ($(window).scrollTop() > 10) {
     $("#editor").removeClass("full");
   } else {
@@ -19,6 +19,14 @@ $(document).on("ready scroll", function() {
 /**
  * Update current year in footer
  */
-$(document).on("ready", function() {
+$(document).on("ready", function () {
+  console.log("Nothing here except pure awesomeness...");
   $("cd").text(new Date().getFullYear());
+});
+
+/**
+ * Scroll on arrow click
+ */
+$(".arrow").on("click", function () {
+  $("html, body").animate({ scrollTop: "50px" }, "fast");
 });
