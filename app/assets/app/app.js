c61d57ba9d64caf7434d849d728753262a65e514
Amir Savand <amir@savandbros.com>
2019-04-16 19:16:36 +0300

    Update code format

--- app/assets/app/app.js
+++ app/assets/app/app.js
@@ -2,31 +2,31 @@
  * Toggle for editor
  */
 setInterval(function () {
-  $("#editor b").toggle();
+  $('#editor b').toggle();
 }, 500);
 
 /**
  * Resize the editor to full screen or minimal based on scroll
  */
-$(document).on("ready scroll", function () {
-  if ($(window).scrollTop() > 10) {
-    $("#editor").removeClass("full");
+$(document).on('ready scroll', function () {
+  if (window.scrollY > 10) {
+    $('#editor').removeClass('full');
   } else {
-    $("#editor").addClass("full");
+    $('#editor').addClass('full');
   }
 });
 
 /**
  * Update current year in footer
  */
-$(document).on("ready", function () {
-  console.log("Nothing here except pure awesomeness...");
-  $("cd").text(new Date().getFullYear());
+$(document).ready(function () {
+  console.info('Nothing here except pure awesomeness...');
+  $('#year').text(new Date().getFullYear());
 });
 
 /**
  * Scroll on arrow click
  */
-$(".arrow").on("click", function () {
-  $("html, body").animate({ scrollTop: "50px" }, "fast");
+$('.arrow').on('click', function () {
+  $('html, body').animate({scrollTop: '50px'}, 250);
 });
